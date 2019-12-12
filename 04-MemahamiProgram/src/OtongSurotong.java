faa9e22e1aed76d2f7288e5b1fc8ea63f23e9497
albi2114 <mohalbis21wew@gmail.com>
2019-12-12 20:16:23 +0700

    java pertemuan 13

---
+++ 04-MemahamiProgram/src/OtongSurotong.java
@@ -0,0 +1,13 @@
+
+/**
+ *
+ * @author albi
+ */
+public class OtongSurotong {
+
+	public static void main(String[] args){
+
+		System.out.print("Nama ku adalah Otong, teman si ucup");
+
+	}
+}
---
+++ 05-Organisasi Program dengan Package/program dengan metode manual/src/programku/OtongSurotong.java
@@ -0,0 +1,15 @@
+package programku;
+/**
+ *
+ * @author albi
+ */
+
+
+public class OtongSurotong {
+
+	public static void main(String[] args){
+
+		System.out.print("Nama ku adalah Otong, teman si ucup");
+
+	}
+}
